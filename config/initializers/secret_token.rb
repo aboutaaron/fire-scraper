e42969a4cd8de398132b61328b4eca5d3eaf662f
aboutaaron <aaron.colby.williams@gmail.com>
2012-09-03 23:10:36 -0700

    added rails stuff

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FireScraper::Application.config.secret_token = '82ad04f26572c6c8df1cb8ee5f0f14fc4842118d6e9c33bf4288199e34e25656011a4323ae94f10710fa336ed1f1b40a4aba3ec39d3e6dde8cbd1f3fb0f2c229'
