26a97a982871aa5722fef23b9abfd9df2aafa28e
aboutaaron <aaron.colby.williams@gmail.com>
2012-09-09 13:16:28 -0700

    added more robo-text and helper to calculate US wildfire numbers

--- app/helpers/fires_helper.rb
+++ app/helpers/fires_helper.rb
@@ -1,2 +1,8 @@
 module FiresHelper
+    def us_fire_totals
+        require "mechanize"
+        a = Mechanize.new
+        a.get("http://www.ncdc.noaa.gov/sotc/fire/")
+        a.page.search("#ytd-fire-stats tr td")[0].text.gsub(",","").to_i
+    end
 end
