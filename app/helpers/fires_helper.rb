7113f55f32468b9b76febb8ac31b2ec57d945cbd
aboutaaron <aaron.colby.williams@gmail.com>
2012-09-26 21:38:54 -0700

    moved SCSS to one file, removed slow helpers

--- app/helpers/fires_helper.rb
+++ app/helpers/fires_helper.rb
@@ -9,26 +9,4 @@ module FiresHelper
         end
         total_acres.inject(:+)
     end
-
-    # Date of data set
-    def date_of_fire_stats
-        a = Mechanize.new
-        a.get("http://www.ncdc.noaa.gov/sotc/fire/")
-        a.page.search("h1").text.gsub("State of the ClimateWildfires","")
-    end
-
-    # Total Acres burned in USA to date
-    def total_acres_burned_usa_to_date
-        a = Mechanize.new
-        a.get("http://www.ncdc.noaa.gov/sotc/fire/")
-        a.page.search("#ytd-fire-stats tr td")[0].text.gsub(",","").to_i
-    end
-
-    # Total number of fires in USA to date
-    def number_of_fires_in_usa_to_date
-        a = Mechanize.new
-        a.get("http://www.ncdc.noaa.gov/sotc/fire/")
-        a.page.search("#ytd-fire-stats tr td")[6].text.gsub(",","").to_i
-    end
-
 end
