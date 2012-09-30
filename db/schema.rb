4988e9d1b107d9e1c78b44c893ab10b5f093f9c1
aboutaaron <aaron.colby.williams@gmail.com>
2012-09-29 23:24:23 -0700

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120927063025) do
+ActiveRecord::Schema.define(:version => 20120930062156) do
 
   create_table "counties", :force => true do |t|
     t.string   "name"
