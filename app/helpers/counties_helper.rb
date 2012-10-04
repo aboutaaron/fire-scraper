c7eec72d1414310f3f8890e7fbc8c719f5bbbee9
aboutaaron <aaron.colby.williams@gmail.com>
2012-10-03 22:03:53 -0700

    created a total fires helper. This should be something in the Controller though

--- app/helpers/counties_helper.rb
+++ app/helpers/counties_helper.rb
@@ -2,4 +2,14 @@ module CountiesHelper
     def total_acres_burned
         number_with_delimiter(@county.fires.sum(:acreage))
     end
+
+    def total_fires_ignited
+        arr =[]
+        @counties.each do |c|
+            c.fires.each do |f|
+                arr << f.name
+            end
+        end
+        arr.count
+    end
 end
