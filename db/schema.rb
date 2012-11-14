0c15664d6e781c19766ef6defe36df1dd15ff8cd
aboutaaron <aaron.colby.williams@gmail.com>
2012-11-14 14:37:13 -0800

    added migration folder

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121029100304) do
+ActiveRecord::Schema.define(:version => 20121114223621) do
 
   create_table "counties", :force => true do |t|
     t.string   "name"
