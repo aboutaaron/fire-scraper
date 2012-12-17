ce73fe5b716d8c52172c5dd673c506265ef7f3e7
aboutaaron <aaron.colby.williams@gmail.com>
2012-12-16 19:40:10 -0800

    removed nofollow link helper from ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,2 @@
 module ApplicationHelper
-    def link_to(name, options = {}, html_options = {})
-        html_options.merge!(:rel => :nofollow)
-        super(name, options, html_options)
-    end
 end
