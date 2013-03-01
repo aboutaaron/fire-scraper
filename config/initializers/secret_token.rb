5fe90403638c23a5b1387a68ec559f2294288b9b
aboutaaron <aaron.colby.williams@gmail.com>
2013-03-01 10:56:37 -0800

    initial rails scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Firescraper::Application.config.secret_token = '9b812233af27dd40177019ea5312775bf33cafe14663ca008cd4854d0cae5aa482e8b739c3346bac028fb172fb81d8060a201bef03593e46223af3a529b20622'
