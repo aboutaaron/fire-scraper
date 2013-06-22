d434790ee21316d4df2354b9807eeb8f55f2e239
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-22 14:49:03 -0700

    created the County table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130622214426) do
+ActiveRecord::Schema.define(:version => 20130622214705) do
+
+  create_table "counties", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "fires", :force => true do |t|
     t.integer  "acreage"
