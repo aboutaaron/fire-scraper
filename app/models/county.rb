3c8d77b4e28adeb431683765d46d32bba77a35a8
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-23 16:23:37 -0700

    added geocoding stuff to model

--- app/models/county.rb
+++ app/models/county.rb
@@ -6,4 +6,11 @@ class County < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
 
   has_many :fires
+
+  geocoded_by :full_address
+  after_validation :geocode
+
+  def full_address
+    name + " County, California, USA"
+  end
 end
