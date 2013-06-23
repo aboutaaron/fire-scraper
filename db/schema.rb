3df96119ea4eadf2d7fe827bb5e7ba6816ea5b6f
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-22 18:33:09 -0700

    ran County migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130623012726) do
+ActiveRecord::Schema.define(:version => 20130623013200) do
+
+  create_table "counties", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "fires", :force => true do |t|
     t.integer  "acreage"
