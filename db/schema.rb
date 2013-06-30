8fb2d67a6d9bb9923f3d818285453261279194a6
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-30 14:45:51 -0700

    containment is now an integer field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130630210112) do
+ActiveRecord::Schema.define(:version => 20130630214328) do
 
   create_table "counties", :force => true do |t|
     t.string   "name"
@@ -26,12 +26,12 @@ ActiveRecord::Schema.define(:version => 20130630210112) do
 
   create_table "fires", :force => true do |t|
     t.integer  "acreage"
-    t.string   "containment"
+    t.integer  "containment", :limit => 255
     t.datetime "date"
     t.string   "location"
     t.string   "name"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",                 :null => false
+    t.datetime "updated_at",                 :null => false
     t.integer  "county_id"
   end
 
