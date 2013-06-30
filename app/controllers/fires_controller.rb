7a7b1cf06229aa12aa403b82b6b5423372710be3
aboutaaron <aaron.colby.williams@gmail.com>
2013-06-30 12:49:06 -0700

    returned fire controller to default

--- app/controllers/fires_controller.rb
+++ app/controllers/fires_controller.rb
@@ -2,27 +2,13 @@ class FiresController < ApplicationController
   # GET /fires
   # GET /fires.json
   def index
-    @county_fires = find_fires
-    @fires = @county_fires.fires
-
+    @fires = Fire.all
     render json: @fires
   end
 
-  # This doesn't work well because Fire ids are created as fires are
-  # Perhaps it's time to rewrite the scrapre
+  # GET /fires/1
+  # GET /fires/1.json
   def show
-    @county_fires = find_fires
-    @fires = @county_fires.fires.find(params[:id])
-
+    @fires = Fires.find(params[:id])
     render json: @fires
-  end
-
-  # http://railscasts.com/episodes/154-polymorphic-association?view=asciicast
-  def find_fires
-    params.each do |name, value|
-      if name =~ /(.+)_id$/
-        return $1.classify.constantize.find(value)
-      end
-    end
-  end
 end
