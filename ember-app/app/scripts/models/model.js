0ff668ce20d65591c6c5b20e3857417e0a24bb11
aboutaaron <aaron.colby.williams@gmail.com>
2013-07-07 17:59:17 -0700

    removed zombie ember data code from model.js

--- ember-app/app/scripts/models/model.js
+++ ember-app/app/scripts/models/model.js
@@ -15,23 +15,3 @@ App.County.reopenClass({
 		});
 	}
 });
-
-// For Ember-Data
-/*
-App.Store = DS.Store.extend()
-
-App.County = Ember.Model.extend({
-	name: DS.attr('string'),
-	latitude: DS.attr('integer'),
-	longitude: DS.attr('integer'),
-	coordinates: function () {
-		var coordinates = [],
-			latitude = this.get('latitude'),
-			longitude = this.get('longitude');
-		coordinates.push(latitude);
-		coordinates.push(longitude);
-
-		return coordinates;
-	}.property('latitude', 'longitude')
-});
-*/
