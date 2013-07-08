67a07f8e5c189a5eb7c201137f3537434b6afa45
aboutaaron <aaron.colby.williams@gmail.com>
2013-07-07 23:27:34 -0700

    stuck on this fucker

--- ember-app/app/scripts/models/model.js
+++ ember-app/app/scripts/models/model.js
@@ -11,5 +11,22 @@ App.County.reopenClass({
 				});
 		});
 		return counties;
+	},
+
+	findAll: function() {
+		var counties = Ember.ArrayProxy.create({ content: [] });
+		$.getJSON("http://0.0.0.0:3000/counties.json").then(
+			function (response) {
+				response.forEach( function (county) {
+					counties.pushObject(App.County.create(county));
+				});
+		});
+		console.log(counties);
+	},
+
+	findCountyBySlug: function (slug) {
+		return this.load().then(function (counties) {
+			return counties.findProperty('slug', slug);
+		});
 	}
 });
--- ember-app/app/scripts/routes/route.js
+++ ember-app/app/scripts/routes/route.js
@@ -5,11 +5,19 @@ App.Router.reopen({
 
 App.Router.map(function () {
   // put your routes here
-  this.resource('counties');
+  this.resource('counties', function () {
+  	this.route('slug', {path: '/:slug' })
+  });
 });
 
 App.CountiesRoute = Ember.Route.extend({
   model: function () {
     return App.County.all();
   }
+});
+
+App.CountiesSlugRoute = Ember.Route.extend({
+	model: function (params) {
+		return this.modelFor('counties').findCountyById(params.slug);
+	}
 });
