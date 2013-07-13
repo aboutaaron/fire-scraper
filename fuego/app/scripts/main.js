709e4bdd023a81c506de54c00a9938fcda374a9e
aboutaaron <aaron.colby.williams@gmail.com>
2013-07-13 16:34:04 -0700

    deleted coffeescript file, add the cali svg and and created Fuego JS object

--- fuego/app/scripts/main.js
+++ fuego/app/scripts/main.js
@@ -1,1 +1,11 @@
-console.log('\'Allo \'Allo!');
+var Fuego = Fuego || {};
+
+Fuego = {
+	init: function () {
+		console.log('Loaded');
+	}
+}
+
+jQuery(document).ready(function($) {
+	Fuego.init();
+});
