7ab796a73892d7ee49094b246482b3acf7448a41
aboutaaron <aaron.colby.williams@gmail.com>
2013-07-15 12:13:57 -0700

    removed migration that was made obsolete in commit [api 68be068]

--- db/migrate/20130630214328_change_containment_to_integer.rb
+++
@@ -1,9 +0,0 @@
-class ChangeContainmentToInteger < ActiveRecord::Migration
-  def up
-  	change_column :fires, :containment, :integer
-  end
-
-  def down
-  	change_column :fires, :containment, :string
-  end
-end
