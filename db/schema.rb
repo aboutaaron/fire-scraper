f72d72d33e844c78ccd30dc96727d0ea25927fa0
aboutaaron <aaron.colby.williams@gmail.com>
2013-08-11 14:33:40 -0700

    ran migration to add active column to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130630214328) do
+ActiveRecord::Schema.define(:version => 20130811212859) do
 
   create_table "counties", :force => true do |t|
     t.string   "name"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20130630214328) do
     t.datetime "created_at",                 :null => false
     t.datetime "updated_at",                 :null => false
     t.integer  "county_id"
+    t.boolean  "active"
   end
 
 end
