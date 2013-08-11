21b9267f117ced25a7b8ddb67a6f9783c1cc0f65
aboutaaron <aaron.colby.williams@gmail.com>
2013-08-11 14:48:01 -0700

    rolled back the last migration

--- db/migrate/20130811214418_change_active_to_fires.rb
+++
@@ -1,9 +0,0 @@
-class ChangeActiveToFires < ActiveRecord::Migration
-  def up
-  	add_column :fires, :active, :boolean, default: false
-  end
-
-  def down
-  	remove_column :fires, :active
-  end
-end
